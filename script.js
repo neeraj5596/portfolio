8698b3ba801f91a2a3614a33ec30a200a05ba3d5
Neeraj <neerajbhanu143@gmail.com>
2025-07-27 21:34:13 +0530

    Commit 2

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-ScrollReveal().reveal('.section', {
+// Add animation to sections when they scroll into view
+ScrollReveal().reveal('section', {
   delay: 200,
   duration: 1000,
+  easing: 'ease-in-out',
   origin: 'bottom',
   distance: '50px',
   reset: true
